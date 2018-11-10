47ce7cdd3fa89247da3df46d8a1bd646f5de70e5
unknown <andruchiha@gmail.com>
2018-11-10 13:18:57 -0500

    as

---
+++ App/screen/upload/index - Copy.js
@@ -0,0 +1,252 @@
+import React, { Component } from 'react';
+import {View,Text,StyleSheet,TextInput,TouchableOpacity,Image} from 'react-native';
+
+import ImagePicker from 'react-native-image-picker';
+import { Dropdown } from 'react-native-material-dropdown';
+
+const options={
+    title:'select a photo',
+    takePhotoButtonTitle:'Take a Photo',
+    chooseFrmoLibraryButtonTitle:'Choose from Gallery',
+    quality:1
+};
+
+
+class InputUsers extends Component{
+
+//constructor have a state that conatains the properties that will recieve the values from Text Inputes
+    constructor(props){
+       super(props)
+        this.state = {
+            TextInputName:'',
+            TextInputEmail:'',
+            TextInputPhoneNumber:'',
+            iamgeSource: null,
+            data: [
+      				{
+      					value: 'Computadoras encendidas',
+      				},
+      				{
+      					value: 'Puerta Abierta',
+      				},
+              {
+      					value: 'llaves perdidas',
+      				},
+      				{
+      					value: 'Retraso de personal',
+      				},
+              {
+      					value: 'alarmas desactivadas',
+      				},
+      				{
+      					value: 'personal no reconocido',
+      				},
+              {
+      					value: 'Agresion',
+      				},
+      				{
+      					value: 'Aulas desordenadas',
+      				},
+              {
+      					value: 'No hay empleados',
+      				},
+      				{
+      					value: 'No llego compa;ero',
+      				},
+              {
+      					value: 'Fugas de gas',
+      				},
+      				{
+      					value: 'Puerta esta cerrada',
+      				},
+      			],
+        }
+    }
+
+    selectPhoto(){
+        ImagePicker.launchCamera(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+              console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            }
+            else {
+              let source = { uri: response.uri };
+              this.setState({
+                iamgeSource: source
+              });
+            }
+          });
+    }
+
+//arrow function that will fire when press on save button to save data in database via API
+InsertUser = ()=>{
+//constant varaibles that equal propertes in state
+const {TextInputName} = this.state;
+const {TextInputEmail} = this.state;
+const {TextInputPhoneNumber} = this.state;
+const {iamgeSource} = this.state;
+
+const formData = new FormData();
+//Add your input data
+formData.append('name', TextInputName);
+formData.append('email', TextInputEmail);
+formData.append('phone_number', TextInputPhoneNumber);
+
+//Add your photo
+//this, retrive the file extension of your photo
+const uriPart = iamgeSource.uri.split('.');
+const fileExtension = uriPart[uriPart.length - 1];
+
+formData.append('iamgeSource', {
+    uri: iamgeSource.uri,
+    name: 'iamgeSource',
+    type: 'image/jpeg',
+});
+
+//API that use fetch to input data to database via backend php script
+fetch('http://192.168.1.109/conex/insert.php',{
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+      body: formData
+  })
+  .then((response) => response.json())
+  .then((responseJson) => {
+   // return responseJson
+     alert(responseJson);
+     this.props.navigation.navigate('home');
+    })
+    .catch((error) => {
+        console.error(error);
+      });
+
+//alert('Pressed!!');
+}
+
+
+    render(){
+        return(
+            <View style ={styles.container}>
+                <TextInput
+                // value = {this.TextInputName}
+                 placeholder = 'Descripcion del incidente'
+                 onChangeText = {TextInputValue=>this.setState({TextInputName:TextInputValue}) }
+                 underlineColorAndroid = 'transparent'
+                 style = {styles.TextInputStyle}
+                />
+
+                <View style={styles.textInput2}>
+                  <Dropdown
+                    label="Seleccion incidente"
+                    data={this.state.data}
+                    onChangeText= {TextInputValue=>this.setState({TextInputEmail:TextInputValue}) }
+                  />
+                </View>
+
+
+
+                <Image style={styles.image}
+                    source={this.state.iamgeSource != null ? this.state.iamgeSource : require('./image/blogSix.png')}
+                />
+
+                <TouchableOpacity style = {styles.TouchableOpacityStyle} onPress={this.selectPhoto.bind(this)}>
+                    <Text style = {styles.TextStyle}>Seleccione la foto</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity activeOpacity = {.4} style = {styles.TouchableOpacityStyle} onPress={this.InsertUser}>
+                    <Text style = {styles.TextStyle}>Enviar incidente</Text>
+                </TouchableOpacity>
+
+
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create ({
+    container : {
+        alignItems:'center',
+        flex:1,
+        marginTop:5,
+        backgroundColor:'#fff'
+    },
+
+    TextInputStyle :{
+        textAlign:'center',
+        marginBottom:7,
+        width:'90%',
+        height:40,
+        borderWidth:1,
+        borderRadius:5,
+        borderColor:'#FF5722'
+    },
+
+    TextInputStyle2 :{
+        textAlign:'center',
+        marginBottom:7,
+        marginTop:20,
+        width:'90%',
+        height:40,
+        borderWidth:1,
+        borderRadius:5,
+        borderColor:'#FF5722'
+    },
+
+    TextStyle : {
+        color:'#fff',
+        textAlign:'center'
+    },
+
+    TouchableOpacityStyle:{
+        paddingTop:10,
+        paddingBottom:10,
+        marginTop:20,
+        borderRadius:5,
+        marginBottom:7,
+        width:'90%',
+        backgroundColor:'#00BCD4'
+    },
+
+    button:{
+        width:250,
+        height:50,
+        backgroundColor:"#330066"
+    },
+
+    text:{
+        color:'white',
+        fontSize:30,
+        textAlign:'center'
+    },
+
+    textInput2: {
+      textAlign:'center',
+      marginBottom:7,
+      marginTop:20,
+      width:'90%',
+      height:40,
+      borderWidth:1,
+      borderRadius:5,
+      borderColor:'#FF5722',
+      paddingBottom:25,
+
+    },
+
+    image:{
+        width:200,
+        height:200,
+        marginTop:30
+    }
+
+
+
+});
+
+export default InputUsers;
--- App/screen/upload/index.js
+++ App/screen/upload/index.js
@@ -1,252 +1,213 @@
-import React, { Component } from 'react';
-import {View,Text,StyleSheet,TextInput,TouchableOpacity,Image} from 'react-native';
-
-import ImagePicker from 'react-native-image-picker';
+import React, {  PropTypes, Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,Button,TextInput,TouchableOpacity,
+  StatusBar,
+  Platform,
+  Image,
+  ImageBackground,
+  ScrollView,
+  BackHandler,
+  I18nManager
+} from 'react-native';
+import {
+  Container,
+  Content,
+  Right,
+  Form,
+  Item,
+  Icon,
+  Label,
+  Input,
+  Header,
+  Left,
+  Body,
+  Title
+} from "native-base";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { Dropdown } from 'react-native-material-dropdown';
 
-const options={
-    title:'select a photo',
-    takePhotoButtonTitle:'Take a Photo',
-    chooseFrmoLibraryButtonTitle:'Choose from Gallery',
-    quality:1
-};
+// Screen Styles
+import styles from "./styles";
+import { Fonts, Metrics, Colors } from "../../Themes/";
 
 
-class InputUsers extends Component{
+export default class register extends Component {
 
-//constructor have a state that conatains the properties that will recieve the values from Text Inputes
-    constructor(props){
-       super(props)
-        this.state = {
-            TextInputName:'',
-            TextInputEmail:'',
-            TextInputPhoneNumber:'',
-            iamgeSource: null,
-            data: [
-      				{
-      					value: 'Computadoras encendidas',
-      				},
-      				{
-      					value: 'Puerta Abierta',
-      				},
-              {
-      					value: 'llaves perdidas',
-      				},
-      				{
-      					value: 'Retraso de personal',
-      				},
-              {
-      					value: 'alarmas desactivadas',
-      				},
-      				{
-      					value: 'personal no reconocido',
-      				},
-              {
-      					value: 'Agresion',
-      				},
-      				{
-      					value: 'Aulas desordenadas',
-      				},
-              {
-      					value: 'No hay empleados',
-      				},
-      				{
-      					value: 'No llego compa;ero',
-      				},
-              {
-      					value: 'Fugas de gas',
-      				},
-      				{
-      					value: 'Puerta esta cerrada',
-      				},
-      			],
-        }
-    }
 
-    selectPhoto(){
-        ImagePicker.launchCamera(options, (response) => {
-            console.log('Response = ', response);
 
-            if (response.didCancel) {
-              console.log('User cancelled image picker');
-            }
-            else if (response.error) {
-              console.log('ImagePicker Error: ', response.error);
-            }
-            else {
-              let source = { uri: response.uri };
-              this.setState({
-                iamgeSource: source
-              });
-            }
-          });
-    }
+  static navigationOptions= ({navigation}) =>({
+  			title:'App Apolys registro',
+  		  header: null,
+  	});
 
-//arrow function that will fire when press on save button to save data in database via API
-InsertUser = ()=>{
-//constant varaibles that equal propertes in state
-const {TextInputName} = this.state;
-const {TextInputEmail} = this.state;
-const {TextInputPhoneNumber} = this.state;
-const {iamgeSource} = this.state;
+	constructor(props){
+		super(props)
+		this.state={
+			userName:'',
+		//	userEmail:'',
+		//	userPassword:'',
+      gender: '',
+      created_at: new Date(),
+      data: [
+				{
+					value: 'Computadoras encendidas',
+				},
+				{
+					value: 'Puerta Abierta',
+				},
+        {
+					value: 'llaves perdidas',
+				},
+				{
+					value: 'Retraso de personal',
+				},
+        {
+					value: 'alarmas desactivadas',
+				},
+				{
+					value: 'personal no reconocido',
+				},
+        {
+					value: 'Agresion',
+				},
+				{
+					value: 'Aulas desordenadas',
+				},
+        {
+					value: 'No hay empleados',
+				},
+				{
+					value: 'No llego compa;ero',
+				},
+        {
+					value: 'Fugas de gas',
+				},
+				{
+					value: 'Puerta esta cerrada',
+				},
+			],
+		}
+	}
 
-const formData = new FormData();
-//Add your input data
-formData.append('name', TextInputName);
-formData.append('email', TextInputEmail);
-formData.append('phone_number', TextInputPhoneNumber);
 
-//Add your photo
-//this, retrive the file extension of your photo
-const uriPart = iamgeSource.uri.split('.');
-const fileExtension = uriPart[uriPart.length - 1];
+	userRegister = () =>{
+		//alert('ok'); // version 0.48
 
-formData.append('iamgeSource', {
-    uri: iamgeSource.uri,
-    name: 'iamgeSource',
-    type: 'image/jpeg',
-});
+		const {userName} = this.state;
+	//	const {userEmail} = this.state;
+	//	const {userPassword} = this.state;
+    const {gender} = this.state;
+    const {created_at} = this.state;
 
-//API that use fetch to input data to database via backend php script
-fetch('http://192.168.100.197/conex/insert.php',{
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'multipart/form-data',
-      },
-      body: formData
-  })
-  .then((response) => response.json())
-  .then((responseJson) => {
-   // return responseJson
-     alert(responseJson);
-     this.props.navigation.navigate('home');
-    })
-    .catch((error) => {
-        console.error(error);
-      });
 
-//alert('Pressed!!');
+		fetch('http://192.168.1.109/conex/register.php', {
+			method: 'post',
+			header:{
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			},
+			body:JSON.stringify({
+				name: userName,
+			//	email: userEmail,
+			//	password: userPassword,
+        gender:gender,
+        created_at:created_at,
+			})
+
+		})
+		.then((response) => response.json())
+			.then((responseJson) =>{
+				alert("incidente Registrado con exito");
+        this.props.navigation.navigate('home');
+			})
+			.catch((error)=>{
+				console.error(error);
+			});
+
+	}
+
+
+
+ render() {
+   StatusBar.setBarStyle("light-content", true);
+
+   if (Platform.OS === "android") {
+     StatusBar.setBackgroundColor("transparent", true);
+     StatusBar.setTranslucent(true);
+   }
+
+   const imageUri =
+     "https://antiqueruby.aliansoftware.net/Images/signin/image_bg_signin_eight.jpg";
+   return (
+     <Container style={styles.container}>
+       <ImageBackground style={styles.imgContainer} source={{ uri: imageUri }}>
+         <Header style={styles.header}>
+           <Left style={styles.left}>
+             <TouchableOpacity
+               style={styles.backArrow}
+               onPress={() => this.props.navigation.navigate('Home')}
+             >
+               <FontAwesome
+                 name={I18nManager.isRTL ? "angle-right" : "angle-left"}
+                 size={30}
+                 color="white"
+               />
+             </TouchableOpacity>
+           </Left>
+           <Body style={styles.body}>
+             <Text style={styles.textTitle}></Text>
+           </Body>
+           <Right style={styles.right} />
+         </Header>
+       </ImageBackground>
+
+
+
+       <ScrollView>
+         <TextInput
+           style={styles.textInput}
+           placeholder="Descripcion del incidente"
+           placeholderTextColor={Colors.shadows}
+           underlineColorAndroid="transparent"
+           autoCapitalize="none"
+           keyboardType="default"
+           textAlign={I18nManager.isRTL ? "right" : "left"}
+           tiniColor={Colors.loginBlue}
+           onChangeText= {userName => this.setState({userName})}
+
+         />
+
+
+         <View style={styles.textInput2}>
+           <Dropdown
+             label="Seleccion incidente"
+             data={this.state.data}
+             textColor={'#959595'}
+             onChangeText= {gender => this.setState({gender})}
+           />
+         </View>
+
+
+         <TouchableOpacity
+           style={styles.buttonSignUp}
+           onPress={this.userRegister}
+         >
+           <Text style={styles.textWhite}>Enviar incidente</Text>
+         </TouchableOpacity>
+
+
+
+
+       </ScrollView>
+     </Container>
+   );
+ }
+
+
 }
 
 
-    render(){
-        return(
-            <View style ={styles.container}>
-                <TextInput
-                // value = {this.TextInputName}
-                 placeholder = 'Descripcion del incidente'
-                 onChangeText = {TextInputValue=>this.setState({TextInputName:TextInputValue}) }
-                 underlineColorAndroid = 'transparent'
-                 style = {styles.TextInputStyle}
-                />
-
-                <View style={styles.textInput2}>
-                  <Dropdown
-                    label="Seleccion incidente"
-                    data={this.state.data}
-                    onChangeText= {TextInputValue=>this.setState({TextInputEmail:TextInputValue}) }
-                  />
-                </View>
-
-
-
-                <Image style={styles.image}
-                    source={this.state.iamgeSource != null ? this.state.iamgeSource : require('./image/blogSix.png')}
-                />
-
-                <TouchableOpacity style = {styles.TouchableOpacityStyle} onPress={this.selectPhoto.bind(this)}>
-                    <Text style = {styles.TextStyle}>Seleccione la foto</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity = {.4} style = {styles.TouchableOpacityStyle} onPress={this.InsertUser}>
-                    <Text style = {styles.TextStyle}>Enviar incidente</Text>
-                </TouchableOpacity>
-
-
-
-            </View>
-        )
-    }
-}
-
-const styles = StyleSheet.create ({
-    container : {
-        alignItems:'center',
-        flex:1,
-        marginTop:5,
-        backgroundColor:'#fff'
-    },
-
-    TextInputStyle :{
-        textAlign:'center',
-        marginBottom:7,
-        width:'90%',
-        height:40,
-        borderWidth:1,
-        borderRadius:5,
-        borderColor:'#FF5722'
-    },
-
-    TextInputStyle2 :{
-        textAlign:'center',
-        marginBottom:7,
-        marginTop:20,
-        width:'90%',
-        height:40,
-        borderWidth:1,
-        borderRadius:5,
-        borderColor:'#FF5722'
-    },
-
-    TextStyle : {
-        color:'#fff',
-        textAlign:'center'
-    },
-
-    TouchableOpacityStyle:{
-        paddingTop:10,
-        paddingBottom:10,
-        marginTop:20,
-        borderRadius:5,
-        marginBottom:7,
-        width:'90%',
-        backgroundColor:'#00BCD4'
-    },
-
-    button:{
-        width:250,
-        height:50,
-        backgroundColor:"#330066"
-    },
-
-    text:{
-        color:'white',
-        fontSize:30,
-        textAlign:'center'
-    },
-
-    textInput2: {
-      textAlign:'center',
-      marginBottom:7,
-      marginTop:20,
-      width:'90%',
-      height:40,
-      borderWidth:1,
-      borderRadius:5,
-      borderColor:'#FF5722',
-      paddingBottom:25,
-
-    },
-
-    image:{
-        width:200,
-        height:200,
-        marginTop:30
-    }
-
-
-
-});
-
-export default InputUsers;
+AppRegistry.registerComponent('register', () => register);
